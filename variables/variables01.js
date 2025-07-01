0bafabf9ed8c91888537efae7d530db51476eb23
gvserpa <gvieiraserpa@gmail.com>
2025-07-01 19:25:58 +1000

    exercise-variable-js-01

---
+++ variables/variables01.js
@@ -0,0 +1,14 @@
+/* 
+
+Create three variables: one to store your name, one for your age, and another for your city.
+Then, display the following sentence in the console:
+
+My name is [NAME], I am [AGE] years old and I live in [CITY].
+
+*/
+
+const name = 'Gustavo';
+const age = 30;
+const city = 'Sydney';
+
+console.log(`My name is ${name}, I am ${age} years old and I live in ${city}.`)
