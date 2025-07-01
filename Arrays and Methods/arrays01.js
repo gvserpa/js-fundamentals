cdce70b8117160650251618117137a451450d366
gvserpa <100240926+gvserpa@users.noreply.github.com>
2025-07-01 19:55:57 +1000

    Delete Arrays and Methods/arrays01.js

--- Arrays and Methods/arrays01.js
+++
@@ -1,9 +0,0 @@
-/* 
-
-Create an array called fruits with at least 4 fruits. Display the entire array in the console.
-
-*/
-
-const fruits = ['orange', 'apple', 'banana', 'watermelon'];
-
-console.log(fruits)
