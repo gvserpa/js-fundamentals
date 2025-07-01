2f6813094417eae736b4eac5fe470bb05cac725b
gvserpa <100240926+gvserpa@users.noreply.github.com>
2025-07-01 19:56:18 +1000

    Delete Arrays and Methods directory

--- Arrays and Methods/arrays09.js
+++
@@ -1,9 +0,0 @@
-/* 
-
-Join the elements of the fruits array into a single string separated by commas. Display the result using join().
-
-*/
-
-const fruits = ['orange', 'apple', 'banana', 'watermelon'];
-
-console.log(fruits.join())
